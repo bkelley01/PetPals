eab785e174e72bf36a0fe8d66e45283a5fb3b1ec
Ian Sexton <sexton.ian@gmail.com>
2022-04-13 12:04:53 -0400

    add menu button that shows, hides menu

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -3,18 +3,21 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div class="logo">
       <img class="petpals-logo" src="@/images/petpals.png" />
-      <div class="header-options">
+      <!-- <div class="header-options"> commented out for modal menu -->
         <!-- <router-link v-bind:to="{ name: 'home' }" v-if="$store.state.token != ''" >Home</router-link>
         <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''" >&nbsp;|&nbsp;Logout</router-link> -->
-        <p v-on:click="showHideMenu">Menu</p>
-          <ul v-show="showMenu" id="menu">
+        <!-- <p v-on:click="showHideMenu">Menu</p>
+          <ul v-show="showMenu" id="menu-list">
             <li class="menu-item">Profile</li>
             <li class="menu-item">Playdates</li>
             <li class="menu-item">Messages</li>
             <li v-if="$store.state.token != ''" class="menu-item"><router-link v-bind:to="{ name: 'logout' }" >Logout</router-link></li>
             <li v-if="$store.state.token == ''" class="menu-item"><router-link v-bind:to="{ name: 'login' }" >Login</router-link></li>
           </ul>
-      </div>
+      </div> commented out for modal menu -->
+      <menu-panel v-if="showMenu"></menu-panel>
+      <p id="menu-btn" v-on:click="showHideMenu">☰</p>
+      
     </div>
     <div class="menu">
       <div class="my-pets"></div>
@@ -23,7 +26,10 @@
 </template>
 
 <script>
+import menuPanel from '@/components/Menu.vue';
+
 export default {
+  components: { menuPanel },
   data() {
     return {
       showMenu: false,
@@ -37,10 +43,6 @@ export default {
     }
   }
 }
-
-
-
-
 
 </script>
 
@@ -93,6 +95,13 @@ li {
   margin: 0 10px; 
   position: relative;
   }
+
+p#menu-btn {
+  font-size: 25px;
+  padding: 0 3px 0 3px;
+  margin-right: 2%;
+  border: 2px solid black;
+}
 
 
 </style>
---
+++ vue/src/components/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="menu">
+        <ul>
+            <li>Profile</li>
+            <li>Playdates</li>
+            <li>Messages</li>
+            <li v-if="$store.state.token != ''" class="menu-item"><router-link v-bind:to="{ name: 'logout' }" >Logout</router-link></li>
+            <li v-if="$store.state.token == ''" class="menu-item"><router-link v-bind:to="{ name: 'login' }" >Login</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'menu-panel'
+}
+</script>
+
+<style>
+
+</style>
