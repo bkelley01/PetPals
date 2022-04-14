b1fb9bbe30683ee94f75435089b3e2d9305b96db
Brendon Kelley <brendonpkelley@gmail.com>
2022-04-14 11:44:12 -0400

    Database tables

--- java/database/create.sh
+++ java/database/create.sh
@@ -6,4 +6,6 @@ createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/pet.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/playdate.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/playdate.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/pet_personality" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/pet_playdate.sql"
