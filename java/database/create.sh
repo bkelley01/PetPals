1201e76cb989d84f1f27800d62276fa279f25fe1
Brendon Kelley <brendonpkelley@gmail.com>
2022-04-14 10:59:52 -0400

    Database test

--- java/database/create.sh
+++ java/database/create.sh
@@ -5,5 +5,5 @@ psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/pets.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/playdates.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/pet.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/playdate.sql"
