01aa02e8bfa1266d63121b673058ee0cb8420ebf
Brendon Kelley <brendonpkelley@gmail.com>
2022-04-14 11:42:29 -0400

    Database tables

---
+++ java/database/pet_personailty.sql
@@ -0,0 +1,14 @@
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS pet_personality;
+
+CREATE TABLE pet_personality (
+	pet_id int NOT NULL CONSTRAINT fk_pet_id REFERENCES pets(pet_id),
+	personality varchar(20) NOT NULL
+);
+
+INSERT INTO pet_personality (pet_id, personality) VALUES (1, 'Friendly');
+INSERT INTO pet_personality (pet_id, personality) VALUES (1, 'Curious');
+INSERT INTO pet_personality (pet_id, personality) VALUES (1, 'Playful');
+
+COMMIT TRANSACTION;
---
+++ java/database/pet_playdate.sql
@@ -0,0 +1,16 @@
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS pet_playdate;
+
+CREATE TABLE pet_playdate (
+	pet_id int NOT NULL CONSTRAINT fk_pet_id REFERENCES pets(pet_id),
+	playdate_id int NOT NULL CONSTRAINT fk_playdate_id REFERENCES playdates(playdate_id),
+	acceptance_status varchar(20) NOT NULL,
+	PRIMARY KEY (playdate_id, pet_id),
+	CONSTRAINT chk_stat CHECK (acceptance_status in ('Accepted','Declined', 'Pending'))
+);
+
+INSERT INTO pet_playdate (pet_id, playdate_id, acceptance_status) VALUES (1, 1, 'Accepted');
+INSERT INTO pet_playdate (pet_id, playdate_id, acceptance_status) VALUES (1, 2, 'Accepted');
+
+COMMIT TRANSACTION;
