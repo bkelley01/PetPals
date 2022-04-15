06997fdac8b41c6a074a6b3b0a247667a4129906
Malachi Gruenhagen <68450431+nurse-the-code@users.noreply.github.com>
2022-04-15 10:13:06 -0400

    updated SQL schema to have primary composite key for pet_personality

--- java/database/pet_personality.sql
+++ java/database/pet_personality.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS pet_personality;
 
 CREATE TABLE pet_personality (
 	pet_id int NOT NULL CONSTRAINT fk_pet_id REFERENCES pets(pet_id),
-	personality varchar(20) NOT NULL
+	personality varchar(20) NOT NULL,
+	PRIMARY KEY(pet_id, personality)
 );
 
 INSERT INTO pet_personality (pet_id, personality) VALUES (1, 'Friendly');
