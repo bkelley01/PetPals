38d624c2040a7ffef4fcc80bfb7f75ce9ad04887
Edwin Sam Jr <edwinsamjr@gmail.com>
2022-04-18 18:35:52 +0000

    Revert "CSS and AddAvailability Database usability"
    This reverts commit 73370a037a4ab88eebc4bcdac320d72edd88d830

--- vue/src/components/playdates/PlaydatesSection.vue
+++ vue/src/components/playdates/PlaydatesSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="playdates-section-container">
-      <button id="create-playdate"><router-link :to="{ name: 'add-availability' }" id="create-playdate-router">Create Playdate</router-link></button>
+      <button>Create Playdate </button>
       <playdate-filter />
       <!-- <playdates-list /> -->
   </div>
@@ -25,24 +25,4 @@ export default {
     flex-direction: column;
     justify-content: center;
   }
-  #create-playdate {
-    background-color: #3399FF;
-    width: 250px;
-    height: 40px;
-    margin: auto;
-    color: white;
-    text-decoration: none;
-    border: none;
-    border-radius: 5px;
-    font-size: 16px;
-  }
-
-  #create-playdate-router {
-    background-color: #3399FF;
-    width: 100px;
-    margin: auto;
-    color: white;
-    text-decoration: none; 
-  }
-  
 </style>
