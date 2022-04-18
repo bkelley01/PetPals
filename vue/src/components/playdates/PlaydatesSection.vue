ac09e936bb5f11afbd0bcf003fa5794c82f7d725
Edwin Sam <edwinsamjr@gmail.com>
2022-04-18 16:10:37 -0400

    Change to Playdates Section

--- vue/src/components/playdates/PlaydatesSection.vue
+++ vue/src/components/playdates/PlaydatesSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="playdates-section-container">
-      <button @click="() => {this.$router.push('/playdates/create')}">Create Playdate </button>
+      <button id="create-playdate" @click="() => {this.$router.push('/playdates/create')}">Create Playdate </button>
       <playdate-filter />
       <!-- <playdates-list /> -->
   </div>
@@ -26,4 +26,18 @@ export default {
     flex-direction: column;
     justify-content: center;
   }
+
+  #create-playdate {
+    background-color: #3399FF;
+    width: 250px;
+    height: 40px;
+    margin: auto;
+    color: white;
+    text-decoration: none;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+
 </style>
