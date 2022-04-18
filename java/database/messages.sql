0e26f13d30b3e87ae3defdd1cd95449c1dab4029
Malachi Gruenhagen <68450431+nurse-the-code@users.noreply.github.com>
2022-04-18 18:18:35 -0400

    database now supports basic message board

---
+++ java/database/messages.sql
@@ -0,0 +1,23 @@
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS messages;
+DROP SEQUENCE IF EXISTS seq_message_id;
+
+CREATE SEQUENCE seq_message_id
+	INCREMENT BY 1
+	NO MAXVALUE
+	NO MINVALUE
+	CACHE 1;
+  
+CREATE TABLE messages(
+	message_id int DEFAULT nextval('seq_pet_id'::regclass) NOT NULL,
+	user_id int NOT NULL CONSTRAINT fk_user_id REFERENCES users(user_id),
+	pet_id int NULL CONSTRAINT fk_pet_id REFERENCES pets(pet_id),
+	posted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	msg_text varchar(280) NOT NULL
+);
+
+INSERT INTO messages (user_id, pet_id, msg_text)
+VALUES (3, 1, 'The default user left a test message about Spot');
+
+COMMIT TRANSACTION;
